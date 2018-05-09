b82d18352af78fa3b6d450d41851e60f69e04303
ha1f <half.ldk.hiz@gmail.com>
2018-05-09 23:58:15 +0900

    Initial Commit

---
+++ Calculation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculation
+//
+//  Created by はるふ on 2018/05/09.
+//  Copyright © 2018年 はるふ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
